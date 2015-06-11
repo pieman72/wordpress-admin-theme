3e8f678bb0318a677c47748d41422332ccc6fe41
Kenny Harvey <pieman72@gmail.com>
2015-06-11 06:34:00 -0400

    Initial commit of v1.0

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Plugin Name: HS Admin Theme
+Plugin URI: http://harveyserv.ath.cx/wordpress-admin-theme
+Description: A plugin to style the WordPress admin pages (mostly for darker colors).
+Author: Kenny Harvey
+Version: 1.0
+Author URI: http://harveyserv.ath.cx/kenny
+*/
+
+function apply_HS_admin_styles() {
+	if(in_array(get_admin_page_title(), array(
+			"Edit Post"
+			,"Add New Post"
+		))){
+		wp_enqueue_style('hs-admin-theme', plugins_url('hsadmin.css', __FILE__));
+	}
+}
+add_action('admin_enqueue_scripts', 'apply_HS_admin_styles');
+add_action('login_enqueue_scripts', 'apply_HS_admin_styles');
+
+?>
